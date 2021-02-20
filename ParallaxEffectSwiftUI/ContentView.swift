b9d08a9f6462170706cc673ac7b0694ebf25689a
Umer Khan <umar_s66@outlook.com>
2021-02-20 11:51:13 +0500

    Project Setup

---
+++ ParallaxEffectSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ParallaxEffectSwiftUI
+//
+//  Created by Umer Khan on 20/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
